a622fa5b41789f52e46f82cd0da2e81dd997cfc2
Lauren Xie <lrnxie@gmail.com>
2020-09-25 00:39:32 -0400

    Add line chart and select in overview page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,109 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Box from "@material-ui/core/Box";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+} from "recharts";
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(1),
+  },
+  box: {
+    margin: theme.spacing(2, 0),
+  },
+  formControl: {
+    width: 200,
+  },
+}));
+
+const data = [
+  {
+    name: "Apr",
+    income: 3000,
+    expense: 1398,
+  },
+  {
+    name: "May",
+    income: 2000,
+    expense: 9800,
+  },
+  {
+    name: "Jun",
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: "Jul",
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: "Aug",
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: "Sep",
+    income: 3490,
+    expense: 4300,
+  },
+];
 
 export default function Home() {
   const classes = useStyles();
 
+  const [time, setTime] = React.useState("6mn");
+
+  const handleChange = (event) => {
+    setTime(event.target.value);
+  };
+
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Overview
-      </Typography>
+    <Container className={classes.root}>
+      <Typography variant="h4">Overview</Typography>
+
+      <Box display="flex" justifyContent="flex-end" className={classes.box}>
+        <FormControl
+          variant="outlined"
+          className={classes.formControl}
+          size="small"
+        >
+          <InputLabel>Time</InputLabel>
+          <Select value={time} onChange={handleChange} label="Time">
+            <MenuItem value="6mn">Past 6 months</MenuItem>
+            <MenuItem value="12mn">Past 12 months</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Box display="flex" justifyContent="center" className={classes.box}>
+        <LineChart
+          width={730}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="expense" stroke="#FC8181" />
+          <Line type="monotone" dataKey="income" stroke="#68D391" />
+        </LineChart>
+      </Box>
     </Container>
   );
 }
