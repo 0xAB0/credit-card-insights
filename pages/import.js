c1c97446c28bab8fc29228c69c8bd8eccd5b9f57
Lauren Xie <lrnxie@gmail.com>
2020-10-13 17:33:11 -0400

    Finish import commit

--- pages/import.js
+++ pages/import.js
@@ -11,7 +11,6 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
-import Checkbox from "@material-ui/core/Checkbox";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -35,37 +34,6 @@ export default function Import() {
   const classes = useStyles();
   const [rows, setRows] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
-  const [selected, setSelected] = useState([]);
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      setSelected(rows);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, row) => {
-    const selectedIndex = selected.indexOf(row);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, row);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
-  };
-
-  const isSelected = (row) => selected.indexOf(row) !== -1;
 
   const handleCategoryChange = async (event, newValue, rowId) => {
     if (newValue) {
@@ -87,17 +55,15 @@ export default function Import() {
     }
   };
 
-  useEffect(() => {
-    const allRowsId = rows.map((row) => row.rowId);
-
-    setSelected(
-      selected.map((selectedItem) =>
-        allRowsId.includes(selectedItem.rowId)
-          ? rows.find((row) => row.rowId === selectedItem.rowId)
-          : selectedItem
-      )
+  const handleCommit = async () => {
+    const res = await axios.put(
+      `${process.env.NEXT_PUBLIC_API_URL}/import/${router.query.id}/commit`
     );
-  }, [rows]);
+
+    if (res.data.status === "ok") {
+      alert("commit ok");
+    }
+  };
 
   useEffect(() => {
     const fetchData = async (id) => {
@@ -131,105 +97,66 @@ export default function Import() {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell padding="checkbox">
-                <Checkbox
-                  color="primary"
-                  indeterminate={
-                    selected.length > 0 && selected.length < rows.length
-                  }
-                  checked={rows.length > 0 && selected.length === rows.length}
-                  onChange={handleSelectAllClick}
-                />
-              </TableCell>
-              <TableCell padding="none">Date</TableCell>
+              <TableCell>Date</TableCell>
               <TableCell>Description</TableCell>
               <TableCell>Amount</TableCell>
               <TableCell>Category</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => {
-              const isItemSelected = isSelected(row);
-              const labelId = `enhanced-table-checkbox-${row.rowId}`;
+            {rows.map((row) => (
+              <TableRow key={row.rowId}>
+                <TableCell component="th" scope="row">
+                  {row.date}
+                </TableCell>
+                <TableCell>{row.description}</TableCell>
+                <TableCell>{row.amount}</TableCell>
+                <TableCell>
+                  <Autocomplete
+                    value={row.category}
+                    onChange={(event, newValue) => {
+                      handleCategoryChange(event, newValue, row.rowId);
+                    }}
+                    filterOptions={(options, params) => {
+                      const filtered = filter(options, params);
 
-              return (
-                <TableRow
-                  key={row.rowId}
-                  hover
-                  role="checkbox"
-                  aria-checked={isItemSelected}
-                  tabIndex={-1}
-                  selected={isItemSelected}
-                >
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      color="primary"
-                      checked={isItemSelected}
-                      inputProps={{ "aria-labelledby": labelId }}
-                      onChange={(event) => handleClick(event, row)}
-                    />
-                  </TableCell>
-                  <TableCell
-                    component="th"
-                    id={labelId}
-                    scope="row"
-                    padding="none"
-                  >
-                    {row.date}
-                  </TableCell>
-                  <TableCell>{row.description}</TableCell>
-                  <TableCell>{row.amount}</TableCell>
-                  <TableCell>
-                    <Autocomplete
-                      value={row.category}
-                      onChange={(event, newValue) => {
-                        handleCategoryChange(event, newValue, row.rowId);
-                      }}
-                      filterOptions={(options, params) => {
-                        const filtered = filter(options, params);
+                      // Suggest the creation of a new value
+                      if (params.inputValue !== "") {
+                        filtered.push(params.inputValue);
+                      }
 
-                        // Suggest the creation of a new value
-                        if (params.inputValue !== "") {
-                          filtered.push(params.inputValue);
-                        }
-
-                        return filtered;
-                      }}
-                      selectOnFocus
-                      clearOnBlur
-                      handleHomeEndKeys
-                      options={categoryList}
-                      getOptionLabel={(option) => {
-                        // Value selected with enter, right from the input
-                        if (typeof option === "string") {
-                          return option;
-                        }
-                        // Add "xxx" option created dynamically
-                        if (option.inputValue) {
-                          return option.inputValue;
-                        }
-                        // Regular option
-                        return option.title;
-                      }}
-                      size="small"
-                      freeSolo
-                      renderInput={(params) => <TextField {...params} />}
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            })}
+                      return filtered;
+                    }}
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    options={categoryList}
+                    getOptionLabel={(option) => {
+                      // Value selected with enter, right from the input
+                      if (typeof option === "string") {
+                        return option;
+                      }
+                      // Add "xxx" option created dynamically
+                      if (option.inputValue) {
+                        return option.inputValue;
+                      }
+                      // Regular option
+                      return option.title;
+                    }}
+                    size="small"
+                    freeSolo
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
 
       <Box textAlign="right" className={classes.box}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => console.log(selected)}
-        >
-          Confirm
+        <Button variant="contained" color="primary" onClick={handleCommit}>
+          Commit
         </Button>
       </Box>
     </Container>
